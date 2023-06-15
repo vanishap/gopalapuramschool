f69506dfcebbd3d5dc460a11497e51fa95b014a6
vanishap <vanishareddyp@gmail.com>
2023-06-15 10:27:07 -0700

    text area added

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -6,15 +6,17 @@ const HomePage = () => {
   const [year, setYear] = useState('');
   const [profession, setProfession] = useState('');
   const [users, setUsers] = useState([]);
+  const [message, setMessage] = useState('');
 
   
   
   const formHandler =(e)=>{
     e.preventDefault();
-    setUsers([name,year,profession, ...users]);
+    setUsers([name,year,profession,message, ...users]);
     setName('');
     setYear('');
     setProfession('');
+    setMessage('');
   };
 
   return (
@@ -41,9 +43,19 @@ const HomePage = () => {
             onChange={(e)=>setProfession(e.target.value)}/>
         </div>
         <div>
-        <button type='submit'>Add</button>
+        <label>Add your experiences:
+            <textarea placeholder='Share your experience here.' 
+             cols="40" rows="4"
+             value={message}
+             onChange={(e)=>setMessage(e.target.value)}
+             ></textarea>
+          </label>
+        </div>
+        <div>
+        <button type='submit'>Submit</button>
         </div>
         </form>
+        {/** add a text area so that they can give some brief idea about their career  */}
       </div>   
       {users && users.map(user=>{
         return (
